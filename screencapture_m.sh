1d02c049b78b6d4c953a02ed84f48e69acc912bd
Kentoak <43945931+kentoak@users.noreply.github.com>
2022-08-04 15:52:47 +0900

    Update screencapture_m.sh

--- screencapture_m.sh
+++ screencapture_m.sh
@@ -1,5 +1,6 @@
 var="/Users/$USER/sccapture/$(date "+%Y%m%d-%H%M%S").jpg"
 screencapture -m -c -x -t jpg $var
+echo &var#キャプチャしなかったときにこれがないとエラーになる
 osascript << EOF
 set jpg_data to the clipboard as JPEG picture
 set the_file to open for access POSIX path of (POSIX file "$var") with write permission
